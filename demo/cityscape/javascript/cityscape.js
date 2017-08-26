e91150b77ac74e6a57a7acfe425d175d4d50eaf2
Brittany McGarr <brittanymcgarr@nevada.unr.edu>
2017-08-26 01:21:01 +0000

    Adding cityScape Demo

--- demo/cityscape/javascript/cityscape.js
+++ demo/cityscape/javascript/cityscape.js
@@ -11,48 +11,20 @@ function init() {
 	document.body.appendChild( container );
 
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-	camera.position.z = 5;
+	camera.position.z = 500;
+	camera.position.y = 500;
 
 	// scene
 	scene = new THREE.Scene();
 	var ambient = new THREE.AmbientLight( 0xC0C0C0 );
 	scene.add( ambient );
-	var directionalLight = new THREE.DirectionalLight( 0xFFFFFF );
+	var directionalLight = new THREE.DirectionalLight( 0xCECECE );
 	directionalLight.position.set( 0, 0, 1 );
 	scene.add( directionalLight );
 
-	// texture
-	var manager = new THREE.LoadingManager();
-	manager.onProgress = function ( item, loaded, total ) {
-		console.log( item, loaded, total );
-	};
-
-	var texture = new THREE.Texture();
-	var onProgress = function ( xhr ) {
-		if ( xhr.lengthComputable ) {
-			var percentComplete = xhr.loaded / xhr.total * 100;
-			console.log( Math.round(percentComplete, 2) + '% downloaded' );
-		}
-	};
-
 	var onError = function ( xhr ) {};
 
-	var loader = new THREE.ImageLoader( manager );
-	loader.load( 'models/giraffe.png', function ( image ) {
-		texture.image = image;
-		texture.needsUpdate = true;
-	} );
-
-	// model
-	var loader = new THREE.OBJLoader( manager );
-	loader.load( 'models/giraffe.obj', function ( object ) {
-		object.traverse( function ( child ) {
-			if ( child instanceof THREE.Mesh ) {
-				child.material.map = texture;
-			}
-		} );
-		scene.add( object );
-	}, onProgress, onError );
+  setupScene();
 
 	//
 	renderer = new THREE.WebGLRenderer();
@@ -63,6 +35,35 @@ function init() {
 
 	//
 	window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function setupScene() {
+  // Floor
+  var geo = new THREE.PlaneGeometry(2000, 2000, 20, 20);
+  var mat = new THREE.MeshBasicMaterial({color: 0x9db3b5, overdraw: true});
+  var floor = new THREE.Mesh(geo, mat);
+  floor.rotation.x = -90 * Math.PI / 180;
+  scene.add(floor);
+
+  // Original building
+  var geometry = new THREE.CubeGeometry(1, 1, 1);
+  geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0.5, 0));
+  var material = new THREE.MeshNormalMaterial({overdraw: true});
+
+  var cityGeometry = new THREE.Geometry();
+
+  for (var i = 0; i < 300; i++) {
+    var building = new THREE.Mesh(geometry.clone());
+    building.position.x = Math.floor(Math.random() * 200 - 100) * 4;
+    building.position.z = Math.floor(Math.random() * 200 - 100) * 4;
+    building.scale.x  = Math.random() * 50 + 10;
+    building.scale.y  = Math.random() * building.scale.x * 8 + 8;
+    building.scale.z  = building.scale.x;
+    THREE.GeometryUtils.merge(cityGeometry, building);
+  }
+
+  var city = new THREE.Mesh(cityGeometry, material);
+  scene.add(city);
 }
 
 function onWindowResize() {
